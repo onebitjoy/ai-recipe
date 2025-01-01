3697738670606fdc12ab0ba3ed324275c3dfa0f1
Vinay Kumar Kharwar <95994085+onebitjoy@users.noreply.github.com>
2025-01-01 13:05:28 +0530

    Initial Commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <img className="logo" src="../public/chef.png" alt="Chef PNG" />
+        <h1 className="title">Ai Recipe Maker</h1>
+      </div>
+    </header>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer.jsx";
+import Header from "../components/Header.jsx";
+import Main from "../components/Main.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
